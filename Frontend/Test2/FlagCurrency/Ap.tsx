4c92198a43e267cfc0d74c7bfcc3cc84c2eacb8f
Vishalkrmandal <vishalparikshitmandal@gmail.com>
2025-03-29 16:01:59 +0530

    before currency exchange

---
+++ Frontend/Test2/FlagCurrency/Ap.tsx
@@ -0,0 +1,11 @@
+import CurrencyConverter from './CurrencyConverter';
+
+function Ap() {
+    return (
+        <div className="App">
+            <CurrencyConverter />
+        </div>
+    );
+}
+
+export default Ap;
